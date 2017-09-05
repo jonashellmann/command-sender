aabb81d7d0f6d70af03f351a9a402863ea82aabe
Jonas Hellmann <hellmann.jonas@web.de>
2017-09-05 21:59:31 +0200

    Ticket #2: Created seperate methods for executing a command

--- app/src/main/java/de/hellmann/command_sender/ssh/SshCommander.java
+++ app/src/main/java/de/hellmann/command_sender/ssh/SshCommander.java
@@ -26,40 +26,71 @@ public class SshCommander {
     }
 
     public List<String> sendCommandWithKeyAuthentication(
+            String command,
             String username,
             String serverIpAddress,
             int sshPort,
             String privateKeyPath,
             String keyPassphrase) throws JSchException, IOException
     {
+        JSch jsch = initializeJSchObjectForKeyAuthentication(privateKeyPath, keyPassphrase);
+        Session session = initializeSessionForKeyAuthentication(jsch, username, serverIpAddress, sshPort);
+        //String command = "echo \"Sit down, relax, mix yourself a drink and enjoy the show...\" >> /tmp/test.out";
+        ChannelExec channelSsh = sendCommand(session, command);
+        List<String> lines = readConsoleOutput(channelSsh);
+        disconnect(channelSsh, session);
+        return lines;
+    }
+
+    public JSch initializeJSchObjectForKeyAuthentication(
+            String privateKeyPath,
+            String passphrase) throws JSchException
+    {
         JSch jsch = new JSch();
-        jsch.addIdentity(privateKeyPath, keyPassphrase);
+        jsch.addIdentity(privateKeyPath, passphrase);
+        return jsch;
+    }
+
+    public Session initializeSessionForKeyAuthentication(
+            JSch jsch,
+            String username,
+            String serverIpAddress,
+            int sshPort) throws JSchException
+    {
         Session session = jsch.getSession(username, serverIpAddress, sshPort);
         session.setConfig("PreferredAuthentications", "publickey,keyboard-interactive,password");
         java.util.Properties config = new java.util.Properties();
         config.put("StrictHostKeyChecking", "no");
         session.setConfig(config);
-        String command = "echo \"Sit down, relax, mix yourself a drink and enjoy the show...\" >> /tmp/test.out";
+        return session;
+    }
 
+    public ChannelExec sendCommand(Session session, String command) throws JSchException
+    {
         session.connect();
-        ChannelExec channelssh = (ChannelExec) session.openChannel("exec");
-        channelssh.setCommand(command);
-        channelssh.setPty(false);
-        channelssh.connect();
+        ChannelExec channelSsh = (ChannelExec) session.openChannel("exec");
+        channelSsh.setCommand(command);
+        channelSsh.setPty(false);
+        channelSsh.connect();
+        return channelSsh;
+    }
 
-        InputStream stdOut = channelssh.getInputStream();
+    private List<String> readConsoleOutput(ChannelExec channelSsh) throws IOException
+    {
+        InputStream stdOut = channelSsh.getInputStream();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stdOut));
-        List<String> ausgabeZeilen = new ArrayList<>();
-        String zeile = null;
-        while((zeile = bufferedReader.readLine()) != null) {
-            ausgabeZeilen.add(zeile);
+        List<String> lines = new ArrayList<>();
+        String line = null;
+        while((line = bufferedReader.readLine()) != null) {
+            lines.add(line);
         }
+        return lines;
+    }
 
-        channelssh.disconnect();
+    private void disconnect(ChannelExec channelSsh, Session session)
+    {
+        channelSsh.disconnect();
         session.disconnect();
-
-
-        return ausgabeZeilen;
     }
 
 }
