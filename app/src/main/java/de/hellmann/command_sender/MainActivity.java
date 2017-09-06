e263cb44899d855f30d3208594f4560eae35c314
Jonas Hellmann <hellmann.jonas@web.de>
2017-09-07 00:14:31 +0200

    Ticket #1: Presenting buttons in a ListView

--- app/src/main/java/de/hellmann/command_sender/MainActivity.java
+++ app/src/main/java/de/hellmann/command_sender/MainActivity.java
@@ -1,17 +1,17 @@
 package de.hellmann.command_sender;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import de.hellmann.command_sender.ssh.SshCommander;
 
@@ -24,65 +24,68 @@ public class MainActivity extends AppCompatActivity {
     private static final String PRIVATE_KEY_PATH = "";
     private static final String KEY_PASSPHRASE = "";
 
+    private Map<Integer, String> commandToButton = new HashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonSsh = (Button) findViewById(R.id.buttonSsh);
-        final TextView textView = (TextView) findViewById(R.id.textView);
-        buttonSsh.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                SshCommander ssh = new SshCommander();
-                try{
-                    List<String> lines =
-                            ssh.sendCommandWithKeyAuthentication(
-                                    COMMAND,
-                                    USERNAME,
-                                    HOST,
-                                    SSH_PORT,
-                                    PRIVATE_KEY_PATH,
-                                    KEY_PASSPHRASE);
-                    String string = "";
-                    for (String s : lines)
-                    {
-                        string += s + "\n";
-                    }
-                    textView.setText(string);
-                }
-                catch(Exception ex){
-                    String string = ex.getMessage();
-                    textView.setText(string);
-                }
-
-            }
-        });
-
-        /*Button buttonSms = (Button) findViewById(R.id.buttonSms);
-        buttonSsh.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String phoneNumber = "+4917697313425";
-                String message = "Hello, this is a test";
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber));
-                intent.putExtra("sms_body", message);
-                startActivity(intent);
-            }
-        });*/
-
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        createListView();
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private void createListView()
+    {
+        final ListView buttonListView = (ListView) findViewById(R.id.buttonlistView);
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Echo 'Hallo'");
+        list.add("Echo folder content");
+
+        mapButtonsWithCommands();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        buttonListView.setAdapter(adapter);
+        buttonListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                sendCommand(commandToButton.get(position));
+            }
+
+        });
+    }
+
+    private void mapButtonsWithCommands() // TODO: Read from configuration file
+    {
+        commandToButton.put(0, "echo 'Hello World'");
+        commandToButton.put(1, "ls -la");
+    }
+
+    private void sendCommand(String command)
+    {
+        final TextView outputTextView = (TextView) findViewById(R.id.outputTextView);
+
+        SshCommander ssh = new SshCommander();
+        try{
+            List<String> lines =
+                    ssh.sendCommandWithKeyAuthentication(
+                            COMMAND,
+                            USERNAME,
+                            HOST,
+                            SSH_PORT,
+                            PRIVATE_KEY_PATH,
+                            KEY_PASSPHRASE);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String s : lines)
+            {
+                stringBuilder.append(s + "\n");
+            }
+            outputTextView.setText(stringBuilder.toString());
+        }
+        catch(Exception ex){
+            outputTextView.setText(ex.getMessage());
+        }
     }
 }
