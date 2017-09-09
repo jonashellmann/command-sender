c46ecd06f78baef58e3797a98c8d6f48c1159b23
Jonas Hellmann <hellmann.jonas@web.de>
2017-09-09 14:18:58 +0200

    Ticket #8: SQLite used to store data

--- app/src/main/java/de/hellmann/command_sender/MainActivity.java
+++ app/src/main/java/de/hellmann/command_sender/MainActivity.java
@@ -1,8 +1,10 @@
 package de.hellmann.command_sender;
 
-import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,9 +12,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import de.hellmann.command_sender.ssh.CommandConfiguration;
 import de.hellmann.command_sender.ssh.HostConfiguration;
@@ -22,17 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
     private List<HostConfiguration> hostConfigurations;
     private List<CommandConfiguration> commandConfigurations;
+    private SQLiteDatabase database;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        establishConnectionToDatabase();
         readHostConfiguration();
         createListView();
-        /*File file = new File(getFilesDir(), "test.txt");
-        final TextView outputTextView = (TextView) findViewById(R.id.outputTextView);
-        outputTextView.setText(file.toString());*/
+
     }
 
     private void createListView()
@@ -63,11 +65,13 @@ public class MainActivity extends AppCompatActivity {
 
     private List<String> createButtonListFromCommands()
     {
+
         ArrayList<String> list = new ArrayList<>();
         for(int i = 0; i < commandConfigurations.size(); i++){
             list.add(commandConfigurations.get(i).getName());
         }
         return list;
+
     }
 
     private void sendCommand(String command, HostConfiguration hostConfiguration)
@@ -99,8 +103,37 @@ public class MainActivity extends AppCompatActivity {
 
     private void readCommandConfiguration() // TODO: Read from configuration file
     {
-        commandConfigurations.add(new CommandConfiguration("echo 'Hello World'", "Print Hello World", null));
-        commandConfigurations.add(new CommandConfiguration("ls -la", "Show directory content", null));
+
+        commandConfigurations = new ArrayList<>();
+
+        Cursor cursor = database.rawQuery("SELECT * FROM command", null);
+
+        int commandIndex = cursor.getColumnIndex("command");
+        int nameIndex = cursor.getColumnIndex("name");
+        int hostConfigurationIdIndex = cursor.getColumnIndex("hostconfiguration_id");
+
+        if (cursor.moveToFirst()){
+            while(!cursor.isAfterLast()){
+                commandConfigurations.add(
+                        new CommandConfiguration(
+                                cursor.getString(commandIndex),
+                                cursor.getString(nameIndex),
+                                null)); // TODO Read from other table
+                cursor.moveToNext();
+            }
+        }
+        cursor.close();
+
+    }
+
+    private void establishConnectionToDatabase()
+    {
+
+        database = openOrCreateDatabase("SSH", MODE_PRIVATE, null);
+
+        database.execSQL("CREATE TABLE IF NOT EXISTS command (id INT(4), command VARCHAR, name VARCHAR, hostconfiguration_id INT(4))");
+        database.execSQL("CREATE TABLE IF NOT EXISTS host (id INT(4), username VARCHAR, host VARCHAR, sshPort INT(5), privateKeyPath VARCHAR, keyPassphrase VARCHAR, password VARCHAR)");
+
     }
 
 }
